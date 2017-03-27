b003f46c5f636090cd806ca6c2cebc89198bfcfe
Jake Bair <jacob.bair@shavlik.com>
2017-03-27 09:46:16 -0600

    updated react-transform-boilerplate

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,28 +2,33 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-hot-middleware/client',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  module: {
-    loaders: [{
-      test: /\.css$/,
-      loader: "style-loader!css-loader"
-    },{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+    // or devtool: 'eval' to debug issues with compiled output:
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        // necessary for hot reloading with IE:
+        'eventsource-polyfill',
+        // listen to code updates emitted by hot middleware:
+        'webpack-hot-middleware/client',
+        // your code:
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }, {
+            test: /\.js$/,
+            loaders: ['babel'],
+            include: path.join(__dirname, 'src')
+        }]
+    }
 };
